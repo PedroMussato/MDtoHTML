5bd2ebf3017539b938313450ff5164d40f541833
ferzener <me@pedromussato.com>
2024-11-14 02:27:12 -0300

    heading working

---
+++ main.py
@@ -0,0 +1,32 @@
+import sys
+
+outputhtmlfilename = 'out.html'
+
+def writehtmlline(filename, linedata):
+    with open('htmloutput.html','a+') as htmlfile:
+        htmlfile.write(linedata+'\n')
+
+with open(sys.argv[1], 'r') as mdfile:
+    mdlines = mdfile.read().split('\n')
+
+for line in mdlines:
+    # heading parsing
+    if line[:7] == '###### ':
+        html = f'<h6>{line.split('###### ')[1]}<h6>'
+        writehtmlline(outputhtmlfilename, html)
+    elif line[:6] == '##### ':
+        html = f'<h5>{line.split('##### ')[1]}<h5>'
+        writehtmlline(outputhtmlfilename, html)
+    elif line[:5] == '#### ':
+        html = f'<h4>{line.split('#### ')[1]}<h4>'
+        writehtmlline(outputhtmlfilename, html)
+    elif line[:4] == '### ':
+        html = f'<h3>{line.split('### ')[1]}<h3>'
+        writehtmlline(outputhtmlfilename, html)
+    elif line[:3] == '## ':
+        html = f'<h2>{line.split('## ')[1]}<h2>'
+        writehtmlline(outputhtmlfilename, html)
+    elif line[:2] == '# ':
+        html = f'<h1>{line.split('# ')[1]}<h1>'
+        writehtmlline(outputhtmlfilename, html)
+
